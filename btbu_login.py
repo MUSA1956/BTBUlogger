69801a2405a17e5c48fd4cee9404bc30029f3e52
LingZMu <79318799+MUSA1956@users.noreply.github.com>
2025-12-01 21:09:43 +0800

    增加标准输入和标准输出 实现github在线验证 增加过程提示，进度更可观 修改小bug（定义的标准输入只能是单个输入所以之前图方便用print()里面填好几个参数用逗号隔开的不行了，现在改成了字符串拼接） 已使用pyinstaller打包

--- btbu_login.py
+++ btbu_login.py
@@ -10,8 +10,17 @@ from selenium.webdriver.remote.webelement import WebElement
 import http.client
 import webbrowser
 import requests
+import datetime
 
+def normal_input(content):
+    now = datetime.datetime.now()
+    s=input("[{}:{}:{} {}:{}:{}.{}]   {}".format(now.year,now.month,now.day,now.hour,now.minute,now.second,str(now.microsecond)[:2],content))
+    return s
 
+def normal_print(content:str):
+    now=datetime.datetime.now()
+    print("[{}:{}:{} {}:{}:{}.{}]   {}".format(now.year,now.month,now.day,now.hour,now.minute,now.second,str(now.microsecond)[:2],content))
+    pass
 
 def check_load_config():
     #文件配置
@@ -32,13 +41,13 @@ def check_load_config():
 
 def BTBUaccount_setup():
     global config
-    account=input("请输入你的btbu账号")
-    password=input("请输入你等btbu密码")
-    print("你的账号:",account)
-    print("你的密码:", password)
-    confirmation=input("是否确认？（Y,y/N,n）")
+    account=normal_input("请输入你的btbu账号")
+    password=normal_input("请输入你等btbu密码")
+    normal_print("你的账号:"+account)
+    normal_print("你的密码:"+ password)
+    confirmation=normal_input("是否确认？（Y,y/N,n）")
     while confirmation not in "YyNn":
-        confirmation = input("是否确认？（Y,y/N,n）")
+        confirmation = normal_input("是否确认？（Y,y/N,n）")
     if confirmation in ["Y","y"]:
         config.set("accounts","BTBUaccount",account)
         config.set("accounts", "BTBUpassword", password)
@@ -50,13 +59,13 @@ def BTBUaccount_setup():
 
 def account_setup():
     global config
-    account=input("请输入你的账号")
-    password=input("请输入你等密码")
-    print("你的账号:",account)
-    print("你的密码:", password)
-    confirmation=input("是否确认？（Y,y/N,n）")
+    account=normal_input("请输入你的账号")
+    password=normal_input("请输入你的密码")
+    normal_print("你的账号:"+account)
+    normal_print("你的密码:"+ password)
+    confirmation=normal_input("是否确认？（Y,y/N,n）")
     while confirmation not in "YyNn":
-        confirmation = input("是否确认？（Y,y/N,n）")
+        confirmation = normal_input("是否确认？（Y,y/N,n）")
     if confirmation in ["Y","y"]:
         config.set("accounts","account",account)
         config.set("accounts", "password", password)
@@ -67,6 +76,25 @@ def account_setup():
 
 def account_verification():
     global config
+    normal_print("正在验证账号")
+    response=requests.get("https://raw.githubusercontent.com/MUSA1956/BTBUlogger/main/account.txt",stream=True, allow_redirects=True)
+    if response.status_code == 200:
+        account_text=response.text
+        dic={}
+        # print(account_text.split())
+        for i in account_text.split():
+            dic[i.split(":")[0]]=i.split(":")[1]
+        if config["accounts"]["account"] in dic:
+            if config["accounts"]["password"] == dic[config["accounts"]["account"]]:
+                normal_print("账号验证成功")
+            else:
+                normal_print("账号信息无效，请重新设置账号信息。")
+                account_setup()
+                account_verification()
+        else:
+            normal_print("账号信息无效，请重新设置账号信息。")
+            account_setup()
+            account_verification()
     #访问github
     pass
 
@@ -89,6 +117,7 @@ def check_accounts():
         BTBUaccount_setup()
 
 def selenium_setup():
+    normal_print("初始化selenium")
     firefox_service = selenium.webdriver.FirefoxService(executable_path=r"geckodriver.exe",log_output="geckodriver.log",port=4444,service_args=['--marionette-port', '2828'])
     firefox_options = selenium.webdriver.FirefoxOptions()
 
@@ -101,6 +130,7 @@ def selenium_setup():
 
 def site_account_setup():
     global config,Driver
+    normal_print("依据BTBU账号配置网站账号中")
     site_dic={"bb":{"site":"https://bb.btbu.edu.cn/",
                     "account":config["accounts"]["BTBUaccount"],
                     "password":config["accounts"]["BTBUaccount"],
@@ -118,15 +148,16 @@ def site_account_setup():
 
 def open_site(site_name:str):
     global Driver
+    normal_print("正在启动"+site_name)
     account = site_dic[site_name]["account"]
     password = site_dic[site_name]["password"]
     Driver.switch_to.new_window("tab")
     Driver.get(site_dic[site_name]["site"])
     for i in site_dic[site_name]["instructions"]:
-        time.sleep(1)
+        time.sleep(0.5)
         if i[0] == "find_element":
             element=Driver.find_element(i[1],i[2])
-            print(element)
+            # print(element)
         elif i[0] == "send":
             element.send_keys(eval(i[1]))
         elif i[0] == "click":
@@ -142,20 +173,22 @@ def open_site(site_name:str):
 def command_input():
     global Driver,config,site_dic
     while True:
-        commands=input("send your command:").split()
-        command=commands[0]
-        if command == "open":
-            open_site(commands[1])
-        elif command == "help":
-            help_inf=("open <site> #site:bb",
-                      "help #get help",
-                      "exit #end program")
-            for i in help_inf:
-                print(i)
+        commands=normal_input("send your command:").split()
+        if commands:
+            command=commands[0]
+            if command == "open":
+                open_site(commands[1])
+            elif command == "help":
+                help_inf=("open <site> #site:bb",
+                          "help #get help",
+                          "exit #end program")
+                for i in help_inf:
+                    normal_print(i)
 
-        elif command == "exit":
-            Driver.quit()
-            break
+            elif command == "exit":
+                normal_print("退出中")
+                Driver.quit()
+                break
 
 
 
